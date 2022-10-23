30dc761b65d31daf74ffa5eee7f4b4e6e098edfe
dxrn <56146321+dxrn@users.noreply.github.com>
2022-10-22 18:52:03 -0700

    Rename commands

--- README.md
+++ README.md
@@ -7,11 +7,11 @@ Generates an sqlite database from Amazon Seller Central Business Reports.
 2. Copy and paste the path of each month into the ```paths.json``` file. (ex. "January":" /january-seller-report.csv")
 
 ## Generating a database
-```python sr2db.py --c azr 2022```
+```python sr2db.py --d azr 2022```
 
 This will create a database named azr with 2022 as the year.
 
 ## Generating a csv
-```python sr2db.py --d azr.db```
+```python sr2db.py --c azr.db```
 
 This will create a formatted csv of all historical demand per month by child ASIN.
--- sr2db.py
+++ sr2db.py
@@ -3,15 +3,13 @@ from qb import *
 
 def main(args):
 
-    if args.c != None:
+    if args.d != None:
         DATABASE_NAME = args.c[0]
         YEAR = args.c[1]
         create_db(format_data(YEAR), DATABASE_NAME)
-    elif args.d != None:
+    elif args.c != None:
         DATABASE_NAME = args.d[0]
         monthly_demand(DATABASE_NAME)
-    elif args.s != None:
-        pass
     else:
         print("No arguments passed") 
         parser.print_help()     
@@ -19,8 +17,8 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--c', help='Builds database using Amazon Seller Reports, takes two arguments, DATABASE_NAME &  YEAR.', nargs=2)
-    parser.add_argument('--d', help='Builds a csv file containing units ordered per month grouped by Child ASIN, takes argument, DATABASE_NAME.', nargs=1)
+    parser.add_argument('--d', help='Builds database using Amazon Seller Reports, takes two arguments, DATABASE_NAME &  YEAR.', nargs=2)
+    parser.add_argument('--c', help='Builds a csv file containing units ordered per month grouped by Child ASIN, takes argument, DATABASE_NAME.', nargs=1)
 
     args = parser.parse_args()
 
